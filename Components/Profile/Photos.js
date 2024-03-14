d5e420f7bcc7c44bbf3a0b2710896bb52bcfe05c
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2024-03-14 19:39:50 +0200

    Update Photos.js

--- Components/Profile/Photos.js
+++ Components/Profile/Photos.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 
 import styles from './stylesPhotos';
+import PropTypes from 'prop-types';
 
 const Photos = ({photos}) => {
   return (
@@ -13,13 +14,31 @@ const Photos = ({photos}) => {
           ))
           : photos.length > 0 ? photos.map((photo) => (
               <View key={photo.docId} style={styles.photosWrapper}>
-                
+                <Image source={{uri:photo.imageSrc}} style={{width: 320, height: 400}}/>
+
+                <View style={{position: 'absolute', fontWeight: 'bold', flexDirection: 'row', alignItems: 'center'}}>
+                  <Text style={{color: 'white', fontWeight: 'bold', flexDirection: 'row', alignItems: 'center'}}>
+                    <SvgIcon name='heart' fill='currentColor' width={8} height={8} />
+                    {photo.likes.length}
+                  </Text>
+
+                  <Text style={{color: 'white', fontWeight: 'bold', flexDirection: 'row', alignItems: 'center'}}>
+                    <SvgIcon name='comment' fill='currentColor' width={8} height={8} />
+                    {photo.comments.length}
+                  </Text>
+                </View>
               </View>
           )): null
         }
       </View>
+
+      {!photos || (photos.length == 0 && <Text style={{textAlign: 'center', fontSize: 20}}>No posts yet</Text>)}
     </View>
   )
+}
+
+Photos.propTypes = {
+  photos: PropTypes.array
 }
 
 export default Photos;
